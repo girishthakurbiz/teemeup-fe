703f573409c3e3a7358fef5b6c21518dd316bf33
Girish Thakur <girish.thakur@biztecno.net>
2025-09-19 11:47:23 +0530

    changes

--- src/components/ChatIntro.js
+++ src/components/ChatIntro.js
@@ -12,23 +12,13 @@ const ChatIntro = () => (
         />
       </div>
       <div className="chat-gpt-content">
-        <div>Creative Engine</div>
-        <div>
-          "Powered by Creative AI — we turn your words into stunning T-shirt
-          designs in seconds."
+        <div>Describe it. We’ll design it.</div>
+        <div className="chat-gpt-subcontent">
+          "From quick sketches to polished visuals, our AI takes your descriptions and turns them into stunning designs. Fast, Easy, and Effortless."
         </div>
       </div>
     </div>
-    <div className="chat-subtitle2">
-      <div className="chat-gpt-icon2">
-        <img
-          src={gptIcon}
-          alt="icon"
-          style={{ width: "100%", height: "100%", objectFit: "contain" }}
-        />
-      </div>
-      <div>Powered by ChatGPT</div>
-    </div>
+    
   </>
 );
 
